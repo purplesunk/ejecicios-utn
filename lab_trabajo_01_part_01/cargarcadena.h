1df2aee6cdef48a0d00f500eb4387661a62596f1
purplesunk <84647408+purplesunk@users.noreply.github.com>
2023-05-08 21:45:27 -0300

    función para cargar Ints y otra para floats

--- lab_trabajo_01_part_01/cargarcadena.h
+++ lab_trabajo_01_part_01/cargarcadena.h
@@ -1,20 +1,12 @@
 #ifndef CARGARCADENA_H_INCLUDED
 #define CARGARCADENA_H_INCLUDED
 
-#include <iostream>
+void ignoreLine();
 
-void ignoreLine() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF) {
-    }
-}
+void cargarCadena(char *cadena, int tam);
 
-void cargarCadena(char *cadena, int tam) {
-    std::cin.getline(cadena, tam);
-    if (!std::cin) {
-        std::cin.clear();
-        ignoreLine();
-    }
-}
+int cargarInt(const char *pregunta);
+
+float cargarFloat(const char *pregunta);
 
 #endif
