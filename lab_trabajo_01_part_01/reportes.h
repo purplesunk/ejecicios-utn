c69c222b46b590b9ea0fac7214bb9f832f15fb56
purplesunk <84647408+purplesunk@users.noreply.github.com>
2023-05-18 11:14:50 -0300

    Parcial Laboratio 2

--- lab_trabajo_01_part_01/reportes.h
+++ lab_trabajo_01_part_01/reportes.h
@@ -1,104 +1,145 @@
 #ifndef REPORTES_H_INCLUDED
 #define REPORTES_H_INCLUDED
 
-void listarMusicosAlumnos() {
-    ArchivoMusico archivo("musicos.dat");
+void listarBateristas() {
+    ArchivoMusico musicos("musicos.dat");
 
-    int cantMusicos = archivo.contarRegistros();
-
-    if (cantMusicos == -1 || cantMusicos == 0) {
-        std::cout << "Error leyendo el archivo.\n";
+    int cantMusicos = musicos.contarRegistros();
+    if (cantMusicos == -1) {
+        std::cout << "No se pudo leer el archivo.\n";
+        return;
+    } else if (cantMusicos == 0) {
+        std::cout << "No hay músicos registrados en el archivo.\n";
         return;
     }
 
+    int cantBateristas = 0;
     Musico musico;
     for (int i = 0; i < cantMusicos; ++i) {
-        musico = archivo.leerMusico(i);
-        if (musico.getClaustro() == 2) {
-            musico.Mostrar();
+        musico = musicos.leerMusico(i);
+
+        if (musico.getInstrumentro() == 5) {
+            ++cantBateristas;
+            std::cout << " Baterista " << cantBateristas << ": \n";
+            std::cout << "----------------------\n";
+            std::cout << "DNI: " << musico.getDni() << '\n';
+            std::cout << "Nombre: " << musico.getNombre() << '\n';
+            std::cout << "Apellido: " << musico.getApellido() << '\n';
+            std::cout << "Tipo de música: " << musico.getTipoMusica() << '\n';
             std::cout << '\n';
         }
     }
 }
 
-void tipoMusicaXorigen() {
-    ArchivoGeneroMusical archivo("generos.dat");
-
-    int cantGeneros = archivo.contarRegistros();
-
-    if (cantGeneros == -1 || cantGeneros == 0) {
-        std::cout << "Error al leer el archivo.\n";
-        return;
-    }
-
-    int cantGenerosXpais[100] = {};
-
-    GeneroMusical genero;
-    for (int i = 0; i < cantGeneros; ++i) {
-        genero = archivo.leerGeneroMusical(i);
-
-        ++cantGenerosXpais[genero.getPaisOrigen() - 1];
-    }
-
-    std::cout << "Cantidad de Generos por pais de Origen: \n";
-    for (int i = 0; i < 100; ++i) {
-        std::cout << "Pais de origen ";
-        std::cout << i + 1 << ": ";
-        std::cout << cantGenerosXpais[i];
-        std::cout << '\n';
-    }
-}
-
-class Musico2023 {
-   private:
-    int dni;
-    char nombre[30];
-    int claustro;
-
-   public:
-    void Mostrar() {
-        std::cout << dni << " " << claustro << " " << nombre << '\n';
-    }
-
-    void setDni(int d) { dni = d; }
-    void setClaustro(int c) { claustro = c; }
-    void setNombre(const char *n) { strncpy(nombre, n, 30); }
-};
-
-void generarMusicos2023() {
+void instrumentoMenosMusicos() {
     ArchivoMusico musicos("musicos.dat");
 
     int cantMusicos = musicos.contarRegistros();
-
-    Musico musico;
-    Musico2023 musico2023;
-    for (int i = 0; i < cantMusicos; ++i) {
-        musico = musicos.leerMusico(i);
-
-        musico2023.setDni(musico.getDni());
-        musico2023.setNombre(musico.getNombre());
-        musico2023.setClaustro(musico.getClaustro());
-
-        FILE *archivo = fopen("musicos2023.dat", "ab");
-        if (archivo == NULL) {
-            return;
-        }
-
-        fwrite(&musico2023, sizeof(Musico2023), 1, archivo);
-
-        fclose(archivo);
-    }
-
-    FILE *archivo = fopen("musicos2023.dat", "rb");
-    if (archivo == NULL) {
+    if (cantMusicos == -1) {
+        std::cout << "No se pudo leer el archivo.\n";
+        return;
+    } else if (cantMusicos == 0) {
+        std::cout << "No hay músicos registrados en el archivo.\n";
         return;
     }
 
-    while (fread(&musico2023, sizeof(Musico2023), 1, archivo)) {
-        musico2023.Mostrar();
+    int cantMusicosXInstrumento[15] = {};
+
+    Musico musico;
+    for (int i = 0; i < cantMusicos; ++i) {
+        musico = musicos.leerMusico(i);
+
+        ++cantMusicosXInstrumento[musico.getInstrumentro() - 1];
     }
 
-    fclose(archivo);
+    /*
+        int instrumentoMenosMusicos = -1;
+        for (int i = 0; i < 15; ++i) {
+            if (instrumentoMenosMusicos == -1 && cantMusicosXInstrumento[i] != 0) {
+                instrumentoMenosMusicos = i;
+
+            } else if (cantMusicosXInstrumento[i] != 0 &&
+                       cantMusicosXInstrumento[i] < cantMusicosXInstrumento[instrumentoMenosMusicos]) {
+                instrumentoMenosMusicos = i;
+            }
+        }
+    */
+
+    int instrumentoMenosMusicos = 0;
+    for (int i = 0; i < 15; ++i) {
+        if (cantMusicosXInstrumento[i] < cantMusicosXInstrumento[instrumentoMenosMusicos]) {
+            instrumentoMenosMusicos = i;
+        }
+    }
+
+    std::cout << "El intrumento con menos músicos es: " << instrumentoMenosMusicos + 1 << '\n';
+}
+
+// Generar un archivo de nombre instrumentos.dat con los músicos que no tocan guitarra (instrumento principal 1).
+// Cada registro debe tener el siguiente formato:
+// DNI, nombre, apellido y tipo de música
+struct NoGuitarristas {
+   private:
+    int dni;
+    char nombre[30];
+    char apellido[30];
+    int tipoMusica;
+
+   public:
+    void setDni(int i) {
+        dni = i;
+    }
+    void setTipoMusica(int i) {
+        tipoMusica = i;
+    }
+    void setNombre(const char *n) {
+        strcpy(nombre, n);
+    }
+    void setApellido(const char *n) {
+        strcpy(apellido, n);
+    }
+
+    void Mostrar() {
+        std::cout << dni << " " << nombre << " " << apellido << " " << tipoMusica << '\n';
+    }
+};
+
+void generarNoGuitarristas() {
+    ArchivoMusico musicos("musicos.dat");
+
+    int cantMusicos = musicos.contarRegistros();
+    if (cantMusicos == -1) {
+        std::cout << "No se pudo leer el archivo.\n";
+        return;
+    } else if (cantMusicos == 0) {
+        std::cout << "No hay músicos registrados en el archivo.\n";
+        return;
+    }
+
+    Musico musico;
+    NoGuitarristas musicoNoGuitarra;
+    for (int i = 0; i < cantMusicos; ++i) {
+        musico = musicos.leerMusico(i);
+
+        if (musico.getInstrumentro() != 1) {
+            FILE *nuevoArchivo = fopen("instrumentos.dat", "ab");
+            if (nuevoArchivo == NULL) {
+                std::cout << "Error al abrir el archivo. \n";
+                return;
+            }
+
+            musicoNoGuitarra.setDni(musico.getDni());
+            musicoNoGuitarra.setTipoMusica(musico.getTipoMusica());
+            musicoNoGuitarra.setNombre(musico.getNombre());
+            musicoNoGuitarra.setApellido(musico.getApellido());
+
+            fwrite(&musicoNoGuitarra, sizeof(musicoNoGuitarra), 1, nuevoArchivo);
+
+            fclose(nuevoArchivo);
+        }
+    }
+
+    std::cout << "Archivo generado.\n";
 }
 
 #endif
