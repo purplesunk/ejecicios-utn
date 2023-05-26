3500be6fccdd6400af4e353e8260d2a283a6090c
purplesunk <84647408+purplesunk@users.noreply.github.com>
2023-05-26 15:41:58 -0300

    Intento de merge sort

--- 24_05_2023_clase/tarea/parcial.cpp
+++ 24_05_2023_clase/tarea/parcial.cpp
@@ -3,7 +3,11 @@
 
 using namespace std;
 
+
 #include "parcial1.h"
+
+void mergeSort(Proyecto vProyecto[], int tam, int inicio);
+void bubbleSort(Proyecto vProy[], int cant);
 
 /// VAMOS A CARGAR EL ARCHIVO proyectos.dat EN UN VECTOR DINAMICO.
 
@@ -26,7 +30,84 @@ int main() {
         }
     }
 
+
     /// REORDENAR EL VECTOR POR NOMBRE DE PROYECTO
+    // bubbleSort(vProy, cant);
+
+    mergeSort(vProy, cant, 0);
+
+    for (int i = 0; i < cant; i++) {
+        vProy[i].Mostrar();
+    }
+
+    delete[] vProy;
+
+    return 0;
+}
+
+void mergeSort(Proyecto vProyecto[], int tam, int inicio) {
+    if (tam <= 1) {
+        return;
+    } 
+
+    int mitad = tam / 2;
+
+    // sort izquierda: 
+    mergeSort(vProyecto, mitad, inicio);
+
+    // sort derecha:
+    mergeSort(vProyecto, mitad, mitad - 1);
+
+    // merge the thingys:
+    Proyecto *vBuffer = new Proyecto[tam];
+
+    int cantDerecha = 0;
+    int cantIzquierda = 0;
+    bool cmpDerecha = false;
+    
+    for (int i = 0; i < tam - 1; ++i) {
+
+        if (cantDerecha == mitad) {
+            vBuffer [i + 1] = vProyecto[inicio + cantIzquierda];
+            ++cantIzquierda;
+        }
+        if (cantIzquierda == mitad) {
+            vBuffer [i + 1] = vProyecto[inicio + mitad + cantDerecha];
+            ++cantDerecha;
+        }
+
+        if (cmpDerecha) {
+            if (strcasecmp(vProyecto[inicio + mitad + cantDerecha].getNombre(), vProyecto[inicio + cantIzquierda].getNombre()) < 0) {
+                vBuffer[i] = vProyecto[inicio + mitad + cantDerecha];
+                cmpDerecha = false;
+                ++cantDerecha;
+            } else {
+                vBuffer[i] = vProyecto[inicio + cantIzquierda];
+                cmpDerecha = true;
+                ++cantIzquierda;
+            }
+        } else {
+            if (strcasecmp(vProyecto[inicio + cantIzquierda].getNombre(), vProyecto[inicio + mitad + cantDerecha].getNombre()) < 0) {
+                vBuffer[i] = vProyecto[inicio + cantIzquierda];
+                cmpDerecha = true;
+                ++cantIzquierda;
+            } else {
+                vBuffer[i] = vProyecto[inicio + cantDerecha];
+                cmpDerecha = false;
+                ++cantDerecha;
+            }
+        }
+    }
+
+    for (int i = 0; i < tam; ++i) {
+        vProyecto[inicio + i] = vBuffer[i];
+    }
+
+    delete[] vBuffer;
+}
+
+
+void bubbleSort(Proyecto vProy[], int cant) {
     bool cambio = true;
     Proyecto proyectoTmp;
     int vueltas = 1;
@@ -42,12 +123,4 @@ int main() {
         }
         vueltas++;
     }
-
-    for (int i = 0; i < cant; i++) {
-        vProy[i].Mostrar();
-    }
-
-    delete[] vProy;
-
-    return 0;
 }
